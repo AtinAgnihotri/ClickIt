237d91263d4e15040ff31b6acc2957f9617379a7
AtinAgnihotri <atinagnihotri@gmail.com>
2021-07-24 16:40:20 +0530

    Adds basic layout to main view

--- ClickIt/ViewControllers/MainViewController.swift
+++ ClickIt/ViewControllers/MainViewController.swift
@@ -12,6 +12,34 @@ class MainViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addTitle()
+        addClickItButton()
+    }
+    
+    func addTitle() {
+        title = "ClickIt"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func addClickItButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(clickIt))
+    }
+    
+    @objc func clickIt() {
+        print("Click It")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ClickItCell", for: indexPath) as? ClickItItem else {
+            fatalError("Failed to dequeue cell")
+        }
+        cell.clickedImage.image = UIImage(systemName: "camera")
+        cell.caption.text = "Test Caption"
+        return cell
     }
 
 
--- ClickIt/Views/ClickItItem.swift
+++ ClickIt/Views/ClickItItem.swift
@@ -8,15 +8,17 @@
 import UIKit
 
 class ClickItItem: UITableViewCell {
-
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
+    
+    @IBOutlet var clickedImage: UIImageView!
+    @IBOutlet var caption: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
 
