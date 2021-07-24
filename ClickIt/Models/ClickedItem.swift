257459a5289328160dff38155b3356c0f88ee643
AtinAgnihotri <atinagnihotri@gmail.com>
2021-07-24 16:27:18 +0530

    Adds ClickedItem Model

---
+++ ClickIt/Models/ClickedItem.swift
@@ -0,0 +1,14 @@
+//
+//  ClickedItem.swift
+//  ClickIt
+//
+//  Created by Atin Agnihotri on 24/07/21.
+//
+
+import Foundation
+
+struct ClickedItem: Codable {
+    let imageName: String
+    let caption: String
+    let notes: String
+}
